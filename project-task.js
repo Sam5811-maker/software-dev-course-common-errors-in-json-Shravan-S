b9426545a8d62ea58f504e425b6e732ca35dc30f
Shravan Samudrala <shravans5811@gmail.com>
2025-05-07 14:00:13 -0500

    Corrected and provided answers

--- project-task.js
+++ project-task.js
@@ -79,7 +79,20 @@ const invalidBookingJSON = `
 - Keep track of each fix:
   • What was wrong?
   • Why is it a problem in JSON?
-  • What did you change to fix it?
+      Invalid JSON!
+      Error: Parse error on line 1:
+      const invalidBooking
+      ^
+    Expecting 'STRING', 'NUMBER', 'NULL', 'TRUE', 'FALSE', '{', '[', got 'undefined'
+
+    • What did you change to fix it?
+        removed line 1 
+        missing , on line 4 checkInDate
+        missing "" for kay name line 8
+        line 14 undefined is not a valide datatype in JSON, insteda use null
+        line 21 extra , at the end of the array amenities
+        removed line 24 `;
+
 */
 
 
@@ -91,11 +104,20 @@ const invalidBookingJSON = `
 💬 Reflect and answer the following:
 
 1️⃣ What tools or techniques did you use to identify the errors?
+I have used JSONLint online tool to identify the erroes
 
 2️⃣ How did you confirm that your corrected JSON file was valid?
+anfter testing in JSONLint the corrected code showed no errors and said JSON is valid
 
 3️⃣ Which errors were the most difficult to spot? Why?
+especially identifying punctuations and brackets are kind of difficult especially when the data structure is a complex one
 
 4️⃣ What strategies can help you avoid these kinds of errors in the future?
    (e.g., syntax highlighting, linters, writing JSON by example)
+   using 
+   online tools like JSONLint
+   allications like VScode has inbuilt or extensions to detect these kind of defects
+   error stacks in the terminal or the browsers
+   debugging with APIs or consols
+
 */
